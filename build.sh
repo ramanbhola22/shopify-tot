0a1e9e37a748153374cdeab2a59ea44099061baf
Arsh Kalsi <arsh.seraphic@gmail.com>
2023-06-06 11:51:18 +0530

    build flow fixes

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd web
+npm install
+
+cd frontend
+npm install
+export SHOPIFY_API_KEY=3f731947689e7036a17186bff1bea0ab && npm run build
--- web/shopify.js
+++ web/shopify.js
@@ -3,6 +3,11 @@ import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-04";
 
+import * as dotenv from 'dotenv'
+dotenv.config({ path: './../.env' });
+
+console.log(process.env);
+
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
